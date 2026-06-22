ece72c715507987812c16c921915ac972da5c05a
BL-Niranjan <niranjan.manivannan@bridgelabz.com>
2026-06-22 12:37:24 +0530

    initial project deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,18 +2,30 @@
 
 import express from 'express'
 import dotenv from 'dotenv'
+import path from 'path'
 
 import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
 
 const app=express()
 
+const __dirname=path.resolve()
+
 dotenv.config()
 
 const PORT = process.env.PORT || 3000
 
-app.get('/api/auth',authRoutes)
-app.get('/api/messages',messageRoutes)
+app.use('/api/auth',authRoutes)
+app.use('/api/messages',messageRoutes)
+console.log("In dev")
+
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,'../frontend/dist')))
+
+    app.get("*",(_,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
 
 app.listen(PORT,()=>{
     console.log(`Listening to ${PORT} port`)
